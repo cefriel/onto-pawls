17f22c96feb4360640849d239a01fd1eeeaf9833
youssef-yo <youssef.yef91@gmail.com>
2022-11-26 00:46:12 +0100

    modificato ui modal relation RelationInfo
    introdotto alcuni elementi ui per permettere poi la modifica della property di una relazione

--- ui/src/components/sidebar/RelationInfo.tsx
+++ ui/src/components/sidebar/RelationInfo.tsx
@@ -1,16 +1,57 @@
-import React from 'react';
-import { infoRelation } from '../../context';
+import React, { useState, useContext, useEffect } from 'react';
+import { infoRelation, AnnotationStore } from '../../context';
+import { EditFilled } from '@ant-design/icons';
+import { OntoProperty } from '../../api';
+import Select from 'react-select';
 
 interface RelationInfoProps {
     info: infoRelation;
 }
 export const RelationInfo = ({ info }: RelationInfoProps) => {
+    const annotationStore = useContext(AnnotationStore);
+    const [modifyPropertyClass, setModifyPropertyClass] = useState(false);
+    const [properties, setProperties]: [properties: any, setProperties: any] = useState([]);
+    const [iRelationInfoInListPropeprties, setIRelationInfoInListPropeprties] = useState<number>(0);
+    const setNewProperty = useState<OntoProperty>()[1];
+    useEffect(() => {
+        const listLabels = annotationStore.ontoProperties.map((ontoProperty: OntoProperty) => ({
+            value: ontoProperty.id,
+            label: ontoProperty.text,
+        }));
+        setProperties(listLabels);
+        const indexRelationInfo = listLabels.findIndex((r) => r.label === info.ontoProperty.text);
+        // al momento non si fa il controllo dell'id poichè se un'ontologia viene eliminata
+        // e poi ricaricata => verranno assegnati nuovi id alle classi => si perde la coerenza tra
+        // gli id delle classi delle annotazioni e degli id delle classi dell'ontologia.
+        // Il problema non si porrà nel caso si decide che le ontologie non si possono eliminare
+        console.log('Indice: ', indexRelationInfo, ' => ', listLabels[indexRelationInfo]);
+        if (indexRelationInfo !== undefined || indexRelationInfo >= 0) {
+            setIRelationInfoInListPropeprties(indexRelationInfo);
+        }
+    }, [annotationStore.ontoProperties]);
+    const colourStyles = {
+        control: (styles: any) => ({ ...styles, backgroundColor: 'white' }),
+        option: (styles: any) => {
+            return {
+                ...styles,
+                color: 'black',
+                cursor: 'default',
+                zIndex: 100,
+            };
+        },
+    };
+    const ontoPropertyFromId = (id: string) => {
+        return annotationStore.ontoProperties.find((ontoProperty: OntoProperty) => {
+            return ontoProperty.id === id;
+        });
+    };
     return (
         <>
             {info.sourceAnnotation !== undefined && info.targetAnnotation !== undefined ? (
                 <>
+                    <h4>Source Annotation</h4>
                     <p>
-                        <b>Source annotation ID</b>: {info.sourceAnnotation.toString()}
+                        <b>ID</b>: {info.sourceAnnotation.toString()}
                     </p>
                     <p>
                         <b>Class</b>: {info.sourceAnnotation.ontoClass.text}
@@ -21,9 +62,10 @@ export const RelationInfo = ({ info }: RelationInfoProps) => {
                     <p>
                         <b>Page</b>: {info.sourceAnnotation.page}
                     </p>
-                    <p>//////////////////////////</p>
+                    <hr></hr>
+                    <h4>Target Annotation</h4>
                     <p>
-                        <b>Target annotation ID</b>: {info.targetAnnotation.toString()}
+                        <b>ID</b>: {info.targetAnnotation.toString()}
                     </p>
                     <p>
                         <b>Class</b>: {info.targetAnnotation.ontoClass.text}
@@ -34,10 +76,37 @@ export const RelationInfo = ({ info }: RelationInfoProps) => {
                     <p>
                         <b>Page</b>: {info.targetAnnotation.page}
                     </p>
-                    <p>//////////////////////////</p>
-                    <p>
-                        <b>Property</b>: {info.ontoProperty.text}
-                    </p>
+                    <hr></hr>
+                    <h4>Relation details</h4>
+                    <div>
+                        <b>Property</b>:
+                        {modifyPropertyClass ? (
+                            <>
+                                <Select
+                                    options={properties}
+                                    defaultValue={properties[iRelationInfoInListPropeprties]}
+                                    styles={colourStyles}
+                                    onChange={(choice: any) => {
+                                        const resultProperty:
+                                            | OntoProperty
+                                            | undefined = ontoPropertyFromId(choice.value);
+                                        if (resultProperty !== undefined) {
+                                            setNewProperty(resultProperty);
+                                        }
+                                    }}
+                                />
+                            </>
+                        ) : (
+                            <>
+                                {info.ontoProperty.text}
+                                <EditFilled
+                                    onClick={() => {
+                                        setModifyPropertyClass(true);
+                                    }}
+                                />
+                            </>
+                        )}
+                    </div>
                 </>
             ) : (
                 <>
