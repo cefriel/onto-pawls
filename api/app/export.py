f9954dfdb3b3766b75d685101e1ad2ed1a764b8b
youssef-yo <youssef.yef91@gmail.com>
2022-12-30 00:23:17 +0100

    Export migliorato
    Viene effettuato correttamente il binding dei namespace per le ontologie
    caricate dall'utente

--- api/app/export.py
+++ api/app/export.py
@@ -26,10 +26,7 @@ def export_annotations(
     title: str,
     documentPath: str,
     npages: int
-):
-    # servono alcuni dati del pdf:
-    # titolo, #pagine_totali, url
-    
+):    
     data = load_annotations(annotationPath)
 
     global annotations
@@ -106,26 +103,24 @@ def analyze_document(title: str, documentPath: str, npages: int, g: Graph):
 
 def analyze_topicAnnotation(i: int, object: Any, g: Graph):
     subject = "annotation" + str(i)
-    print("subject: ", subject)
     s = URIRef(ex[subject])
     
-    hasPage = URIRef(kh_a["hasPage"])
-    hasSnippet = URIRef(kh_a["hasSnippet"])
-    hasData = URIRef(dc["created"])
-    hasTopic = URIRef(kh_a["hasTopic"])
+    hasPage = kh_a["hasPage"]
+    hasSnippet = kh_a["hasSnippet"]
+    hasData = dc["created"]
+    hasTopic = kh_a["hasTopic"]
 
-    topicAnnotation = URIRef(kh_a["TopicAnnotation"])
+    topicAnnotation = kh_a["TopicAnnotation"]
     # number of page starts counting from zero
     page = Literal(object['page'] + 1)
     snippet = Literal(object['text'])
     dataCreation = Literal(object['date'])
-    topic = URIRef(ex[object["id"]])
+    topic = ex[object["id"]]
 
     g.add((s, RDF.type, topicAnnotation))
     g.add((s, hasPage, page))
     g.add((s, hasSnippet, snippet))
     g.add((s, hasData, dataCreation))
-    # manca collegamento con AUTORE
     g.add((s, hasTopic, topic))
     g.add((s, PROV.wasAttributedTo, user))
 
@@ -134,31 +129,30 @@ def analyze_topicAnnotation(i: int, object: Any, g: Graph):
     analyze_annotation(object, g)
 
 def getUrirefUserOntology(iri: str, g: Graph):
-    print("IRI: ", iri)
-    print("initial user_ontologies: ", user_ontologies)
     if "#" in iri:
         baseIri = iri.split("#")[0]
         element = iri.split("#")[-1]
-        print("# is in iri. BaseIri=", baseIri)
+        
         if baseIri in user_ontologies:
             return user_ontologies[baseIri], element
         else:
             nameOnto = baseIri.split("/")[-1]
-            print("nameOnto = ", nameOnto)
-            ns = Namespace(baseIri)
-            user_ontologies.update({baseIri: URIRef(ns)})
+            
+            ns = Namespace(baseIri + "#")
+            user_ontologies.update({baseIri: ns})
             g.bind(nameOnto, ns)
-            print("final user_ontologies: ", user_ontologies, " type: ", type(user_ontologies[baseIri]))
 
             return user_ontologies[baseIri], element
-    else:
+    else: # in this case we have url/class
         baseIri = iri.rsplit('/', 1)[0]
         element = iri.split("/")[-1]
+
         if baseIri in user_ontologies:
             return user_ontologies[baseIri], element
         else:
             nameOnto = iri.split("/")[-1]
-            ns = Namespace(baseIri)
+
+            ns = Namespace(baseIri + "/")
             user_ontologies.update({baseIri: URIRef(ns)})
             g.bind(nameOnto, ns)
 
@@ -166,9 +160,9 @@ def getUrirefUserOntology(iri: str, g: Graph):
 
 def analyze_annotation(object: Any, g: Graph):
     s = URIRef(ex[object["id"]])
-    o = URIRef(object['ontoClass']['iri'])
 
-    #object_uriref, nameClass = getUrirefUserOntology(object['ontoClass']['iri'], g)
+    object_uriref, nameClass = getUrirefUserOntology(object['ontoClass']['iri'], g)
+    o = object_uriref[nameClass]
 
     comment = Literal(object['text'])
     
@@ -183,9 +177,10 @@ def analyze_relation(object: Any, g: Graph):
     target_annotation = get_annotation_by_id(target_annotation_id)
     
     if source_annotation is not None and target_annotation is not None:
-        print("source_annotation: ", source_annotation['id'], "\t target_annotation: ", target_annotation['id'])
+        predicate_uriref, nameProperty= getUrirefUserOntology(object['ontoProperty']['iri'], g)
+
         s = URIRef(ex[source_annotation_id])
-        p = URIRef(object['ontoProperty']['iri'])
+        p = predicate_uriref[nameProperty]
         o = URIRef(ex[target_annotation_id])
         
         g.add((s, p, o))
